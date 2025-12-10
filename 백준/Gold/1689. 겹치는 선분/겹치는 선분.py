57d474f7fabf6a25696c07507da24d6d2c47015c
llabkk <kscheol25@gmail.com>
2025-12-10 21:28:29 +0900

    [Gold IV] Title: 겹치는 선분, Time: 4776 ms, Memory: 252348 KB -BaekjoonHub

--- 백준/Gold/1689. 겹치는 선분/겹치는 선분.py
+++ 백준/Gold/1689. 겹치는 선분/겹치는 선분.py
@@ -1,23 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-from heapq import heappop, heappush
+N = int(input())
+points = []
 
-N = int(input().strip())
-lines = []
 for _ in range(N):
     s, e = map(int, input().split())
-    heappush(lines, (s, 1))
-    heappush(lines, (e, -1))
+    points.append((s, 1))   # 시작점: +1
+    points.append((e, -1))  # 끝점: -1
 
-answer = tmp = 0
+points.sort()  # 좌표 기준, 같은 좌표면 -1이 1보다 먼저 옴
 
-
-while lines:
-    _, score = heappop(lines)
-
-    tmp += score
-
-    answer = max(answer, tmp)
+cnt = 0
+answer = 0
+for x, v in points:
+    cnt += v          # 현재 위치에서 겹치는 선분 개수
+    if cnt > answer:
+        answer = cnt  # 최대값 갱신
 
 print(answer)
