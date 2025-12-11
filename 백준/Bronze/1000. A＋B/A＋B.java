b12985909706ed7c18e60ce185f7e3cd9044e2d7
llabkk <kscheol25@gmail.com>
2025-12-11 09:56:30 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] parts = br.readLine().split(" ");
+        int a = Integer.parseInt(parts[0]);
+        int b = Integer.parseInt(parts[1]);
+		System.out.println(a + b);
+	}
+}
