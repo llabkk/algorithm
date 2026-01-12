a092ef28b91a6d1256cf80d85eefdbffde84b893
llabkk <kscheol25@gmail.com>
2026-01-12 15:30:50 +0900

    [Silver II] Title: 유턴 싫어, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2823. 유턴 싫어/유턴 싫어.py
+++ 백준/Silver/2823. 유턴 싫어/유턴 싫어.py
@@ -1,45 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-from collections import deque
-
 r, c = map(int, input().split())
 
 grid = [input().strip() for _ in range(r)]
 
 delta = ((-1, 0), (0, -1), (1, 0), (0, 1))
 
-def bfs(row, col):
-    que = deque([(row, col)])
-
-    visited = [[0] * c for _ in range(r)]
-    visited[row][col] = 1
-    
-    while que:
-        y, x = que.popleft()
-
-        path = 0
-        for dy, dx in delta:
-            ny = y + dy
-            nx = x + dx
-
-            if 0 <= ny < r and 0 <= nx < c and grid[ny][nx] == '.':
-                path += 1
-                if not visited[ny][nx]:
-                    que.append((ny, nx))
-                    visited[ny][nx] = 1
-        else:
-            if path <= 1:
-                return 1
-    return 0
-
-answer = -1
+answer = 0
 for i in range(r):
     for j in range(c):
+        path = 0
         if grid[i][j] == '.':
-            answer = bfs(i, j)
-            break
-    if answer != -1:
-        break
-
+            for di, dj in delta:
+                ni, nj = i + di, j + dj
+                if 0 <= ni < r and 0 <= nj < c and grid[ni][nj] == '.':
+                    path += 1
+            if path <= 1:
+                answer = 1
 print(answer)
