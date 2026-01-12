99ceac706505ba334d8f081992bec45c95a210af
llabkk <kscheol25@gmail.com>
2026-01-12 23:25:58 +0900

    [Gold V] Title: 감시 피하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.py
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.py
@@ -9,12 +9,23 @@ grid = [input().split() for _ in range(n)]
 
 delta = ((0, 1), (1, 0), (-1, 0), (0, -1))
 
-target = []
+target = set()
 
 for i in range(n):
     for j in range(n):
-        if grid[i][j] == 'X':
-            target.append((i, j))
+        if grid[i][j] == 'T':
+            for di, dj in delta:
+                ni, nj = i + di, j + dj
+                tmp = []
+                while 0 <= ni < n and 0 <= nj < n:
+                    if grid[ni][nj] == 'S':
+                        for t in tmp:
+                            target.add(t)
+                        break
+                    if grid[ni][nj] == 'X':
+                        tmp.append((ni, nj))
+                    ni += di
+                    nj += dj
 
 answer = "NO"
 
@@ -35,13 +46,18 @@ def dfs():
     answer = "YES"
     return
 
-for a, b, c in combinations(target, 3):
-    grid[a[0]][a[1]] = 'O'
-    grid[b[0]][b[1]] = 'O'
-    grid[c[0]][c[1]] = 'O'
+if len(target) < 3:
+    for a in target:
+        grid[a[0]][a[1]] = 'O'
     dfs()
-    grid[a[0]][a[1]] = 'X'
-    grid[b[0]][b[1]] = 'X'
-    grid[c[0]][c[1]] = 'X'
+else:
+    for a, b, c in combinations(target, 3):
+        grid[a[0]][a[1]] = 'O'
+        grid[b[0]][b[1]] = 'O'
+        grid[c[0]][c[1]] = 'O'
+        dfs()
+        grid[a[0]][a[1]] = 'X'
+        grid[b[0]][b[1]] = 'X'
+        grid[c[0]][c[1]] = 'X'
 
 print(answer)
