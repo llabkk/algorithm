4b9bcee55b22b354a36b42a67989ec95de56a019
llabkk <kscheol25@gmail.com>
2026-01-24 13:19:11 +0900

    [Silver V] Title: 생일, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.py
+++ 백준/Silver/5635. 생일/생일.py
@@ -3,28 +3,9 @@ input = sys.stdin.readline
 
 n = int(input().strip())
 
-old = (0, 1, 1, 2011)
-young = (0, 31, 12, 1989)
+people = [input().split() for _ in range(n)]
 
-for _ in range(n):
-    name, day, month, year = input().split()
-    day = int(day)
-    month = int(month)
-    year = int(year)
+people.sort(key=lambda x: (int(x[3]), int(x[2]), int(x[1])))
 
-    if old[3] > year:
-        old = (name, day, month, year)
-    elif old[3] == year and old[2] > month:
-        old = (name, day, month, year)
-    elif old[3] == year and old[2] == month and old[1] > day:
-        old = (name, day, month, year)
-
-    if young[3] < year:
-        young = (name, day, month, year)
-    elif young[3] == year and young[2] < month:
-        young = (name, day, month, year)
-    elif young[3] == year and young[2] == month and young[1] < day:
-        young = (name, day, month, year)
-
-print(young[0])
-print(old[0])
+print(people[-1][0])
+print(people[0][0])
