fd5c77b6e8b56a4a1f8b933c8eabf055ca55eb74
llabkk <kscheol25@gmail.com>
2026-02-04 22:40:08 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,25 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-from collections import deque
-
 n, k = map(int, input().split())
 
-lst = deque(list(range(1, n + 1)))
-
-answer = '<'
-count = 0
+lst = list(range(1, n + 1))
+answer = []
+idx = 0
 while lst:
-    tmp = lst.popleft()
-    count += 1
+    idx = (idx + k - 1) % len(lst)
+    answer.append(lst.pop(idx))
 
-    if count < k:
-        lst.append(tmp)
-    else:
-        count = 0
-        if lst:
-            answer += str(tmp) + ', '
-        else:
-            answer += str(tmp) + '>'
-
-print(answer)
+print('<' + ', '.join(map(str, answer)) + '>')
